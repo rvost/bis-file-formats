fbc9dd7f6b566eebf21383fc6c8f6e7730efd012
rvost <romanvost@gmail.com>
2023-03-20 19:52:39 +0300

    Add PBO checksum calculation

---
+++ BIS.Signatures.Test/PboExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿namespace BIS.Signatures.Test
+{
+    public class PboExtensionsTests
+    {
+        [Fact]
+        public void CanReadChecksum()
+        {
+            var pbo = new PBO.PBO(@"Resources\test.pbo");
+            var expected = new byte[20] { 0x8, 0x64, 0x1A, 0x93, 0x4B, 0x26, 0xFE, 0xC6, 0x8E, 0x30, 0x7D, 0x3A, 0xBC, 0xBC, 0xC3, 0xC5, 0x35, 0xB7, 0xAB, 0x35 };
+
+            var actual = pbo.ReadChecksum();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CanCalculateChecksum()
+        {
+            var pbo = new PBO.PBO(@"Resources\test.pbo");
+            var expected = new byte[20] { 0x8, 0x64, 0x1A, 0x93, 0x4B, 0x26, 0xFE, 0xC6, 0x8E, 0x30, 0x7D, 0x3A, 0xBC, 0xBC, 0xC3, 0xC5, 0x35, 0xB7, 0xAB, 0x35 };
+
+            var actual = pbo.CalculateChecksum();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ BIS.Signatures/PboExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using Pbo = BIS.PBO.PBO;
+
+namespace BIS.Signatures
+{
+    public static class PboExtensions
+    {
+
+        public static byte[] CalculateChecksum(this Pbo pbo)
+        {
+            using var buffer = new MemoryStream();
+            pbo.PBOFileStream.CopyTo(buffer);
+            buffer.SetLength(buffer.Length - 21);
+            buffer.Position = 0;
+
+            using var sha = SHA1.Create();
+            return sha.ComputeHash(buffer);
+        }
+        public static byte[] ReadChecksum(this Pbo pbo)
+        {
+            var fs = pbo.PBOFileStream;
+
+            var pos = fs.Position;
+            fs.Seek(-21, System.IO.SeekOrigin.End);
+            var check = fs.ReadByte();
+            var buffer = new byte[20];
+            fs.Read(buffer, 0, 20);
+            fs.Position = pos;
+
+            if (check == 0)
+            {
+                return buffer;
+            }
+            else
+            {
+                // TODO: Use domain exception
+                throw new InvalidOperationException("PBO file does not contain signature");
+            }
+        }
+    }
+}
