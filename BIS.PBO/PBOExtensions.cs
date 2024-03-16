4df3e2dcf00359f5908637909f60027e63d265f6
Julien <julien.etelain@outlook.com>
2024-03-16 15:23:41 +0100

    Convenience methods for config

---
+++ BIS.PBO/PBOExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using BIS.Core.Config;
+
+namespace BIS.PBO
+{
+    public static class PBOExtensions
+    {
+        public static ParamFile ReadAsConfig(this IPBOFileEntry entry)
+        {
+            using (var stream = entry.OpenRead())
+            {
+                return new ParamFile(stream);
+            }
+        }
+
+        public static ParamFile GetRootConfig(this PBO pbo)
+        {
+            var configEntry = pbo.Files.FirstOrDefault(f => string.Equals(f.FileName, "config.bin", StringComparison.OrdinalIgnoreCase));
+            if (configEntry != null)
+            {
+                return configEntry.ReadAsConfig();
+            }
+            return null;
+        }
+    }
+}
